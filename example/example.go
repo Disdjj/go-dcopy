cc06155d02afc2bb0eb4647be0fdbf4bc5220a3f
djj <shuaiqijianhao@qq.com>
2024-01-27 17:39:25 +0800

    [mod] change module

--- example/example.go
+++ example/example.go
@@ -3,20 +3,20 @@ package main
 import (
 	"fmt"
 
-	"github.com/Disdjj/go-dcopy"
+	d "github.com/Disdjj/go-dcopy"
 )
 
 func main() {
 	a := 1
-	i := go_dcopy.Copy(a).(int)
+	i := d.Copy(a).(int)
 	println(i)
 
 	b := "hello"
-	s := go_dcopy.Copy(b).(string)
+	s := d.Copy(b).(string)
 	println(s)
 
 	m := map[string]int{"one": 1, "two": 2}
-	m2 := go_dcopy.Copy(m).(map[string]int)
+	m2 := d.Copy(m).(map[string]int)
 
 	// 修改m, m2不会变化
 	m["three"] = 3
